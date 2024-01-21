3f48ec4705640ed4e9a4884d2772eade96f89169
yavrum13 <yuval.avrum@gmail.com>
2024-01-20 18:43:08 -0600

    Object of Employees, methods practice

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+  name: 'Sam',
+  streetAddress: '11 Broadway'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  const employeeInfo = {...employee};
+  employeeInfo[key] = value;
+  return employeeInfo;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+  obj[key] = value;
+  return obj;
+}
+
+const newAddress = destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, '12 Broadway');
+
+function deleteFromEmployeeByKey(employee, key, value){
+  const newEmployee = {...employee};
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+  const newEmployee = employee;
+  newEmployee[key] = value;
+  return newEmployee;
+}
